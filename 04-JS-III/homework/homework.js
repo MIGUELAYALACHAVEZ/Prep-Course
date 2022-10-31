36645a5c5ab959852e1d72ec4d58a7d6790c690a
MIguel A. Chávez <miguel@Miguel>
2022-10-31 12:05:13 -0400

    :D

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -55,12 +55,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-  var cadena="";
-  for(var i=0;i<palabras.length;i++){
-    cadena=cadena+palabras[i];
-    cadena=cadena+" ";
-  }
-  return cadena;
+
 }
 
 function arrayContiene(array, elemento) {
@@ -100,13 +95,18 @@ function promedioResultadosTest(resultadosTest) {
   return promedio;
 }
 
-
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var i=0,aux=0;
+  for(i=0;i<numeros.length;i++){
+    if(numeros[i]>aux && aux<numeros[i]){
+      aux=numeros[i];
+    }
+  }
+  return aux;
 }
-
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
@@ -127,6 +127,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+    switch(true){
+      case ((numeroDeDia==="2")||(numeroDeDia==="3")||(numeroDeDia==="4")||(numeroDeDia==="5")||(numeroDeDia==="6")):
+        return "Es dia Laboral";
+      default:
+        return "Es fin de semana";
+    } 
   
 } 
 
@@ -135,7 +141,7 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+
 }
 
 
@@ -143,6 +149,19 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  var i,r=0;
+  for(i=0;i<arreglo.length;i++){
+    if(arreglo[i]==arreglo[i+1]){
+      r=1;
+    }else{
+      break;
+    }
+  }
+  if(r===1){
+    return true;
+  }else{
+    return false;
+  }
   
 } 
 
@@ -159,6 +178,14 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var i;
+  var v=[];
+  for(i=0;i<array.length;i++){
+    if(array[i]>100){
+      v.push(array[i]);
+    }
+  }
+  return v;
 }
 
 
